b53946c43de9f1d605e9d51d23949db7a4cc8221
avishek0769 <avishekadhikary42@gmail.com>
2025-04-26 20:22:58 +0530

    First Commit

--- android/app/src/main/java/com/findit/MainApplication.kt
+++ android/app/src/main/java/com/findit/MainApplication.kt
@@ -11,13 +11,16 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+// import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
+
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
+        PackageList(this).packages.apply {
+              // add(RNDateTimePickerPackage()) 
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
             }
