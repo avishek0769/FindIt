dbfa90a7ff5fedda15894d398a741ea59aae5fbf
avishek0769 <avishekadhikary42@gmail.com>
2025-06-06 18:57:34 +0530

    Update Status Bar

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/TabNavigator';
+import { StatusBar } from 'react-native';
 
 export default function App() {
 
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor={"white"} showHideTransition={'slide'} barStyle={'dark-content'} />
       <Tabs />
     </NavigationContainer>
   );
